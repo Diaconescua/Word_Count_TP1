2146e7b1ad9dcad2b481bfaa0c29c9a348c060a7
Diaconescua <143746197+Diaconescua@users.noreply.github.com>
2023-09-13 11:48:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# fonction qui divise la phrase en mots, puis returne le nombre de mots
+# Alexei Diaconescu
+# Word_Count
+
+# tu ecris une phrase
+# fonction qui divise la phrase en mots, puis returne le nombre de mots.
+# cela te donne le nombre de mots.
+
 def word_count(str):
     return len(str.split())
-
-
-words = input("write: ")
-# je print le nombre de mots
-print("You just wrote", word_count(words), "words.")
